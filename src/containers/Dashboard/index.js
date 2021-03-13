5ee62f2d3054d6bd81333a8c132d28344fafe93c
Oleg Kovalchuk <patriot24081991oleg@gmail.com>
2021-03-13 22:59:44 +0500

    modified:   src/containers/Dashboard/index.js

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -2,14 +2,26 @@ import React, { useState } from 'react';
 import { Box, Button } from '@material-ui/core';
 import useStyles from './style';
 import { DiagnosticsDialog } from '@app/components/Custom';
-
+import Typography from '@material-ui/core/Typography';
 const DashboardContainer = () => {
   const classes = useStyles();
   const [openCreate, setOpenDialog] = useState(false);
   return (
     <Box className={classes.root}>
-      Dashboard Container
-      <Button onClick={() => setOpenDialog(true)}>View Diagnostics</Button>
+      <Typography l="4rem" variant="h4" gutterBottom>
+        Dashboard Container
+      </Typography>
+
+      <Typography variant="button" display="block" gutterBottom>
+        <Button
+          l="4rem"
+          onClick={() => setOpenDialog(true)}
+          color="primary"
+          variant="contained"
+        >
+          View Diagnostics
+        </Button>
+      </Typography>
       <DiagnosticsDialog
         open={openCreate}
         title="Diagnostics"
